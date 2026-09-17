719794d902cd815987247243a1ee9aeef0bb9a96
THANIGAIVEL5 <kthanigaivel2503@gmail.com>
2026-09-17 09:35:29 +0530

    Remove unwanted duplicate folders, heavy media docs, and unused scripts to drastically reduce repository and Docker context size

--- models/gods-eye-view-main/gods-eye-view-main/src/standalone/tools.js
+++
@@ -1,8 +0,0 @@
-import { createApplicationTools } from '../app/tools.js';
-import { startStandaloneChrome } from './startupChrome.js';
-export function createStandaloneTools(options) {
-  return createApplicationTools({
-    startChrome: startStandaloneChrome,
-    ...options,
-  });
-}
